509620649f1a7d04a16605ce59bf4faa1e20b3b4
GoodBoyNeon <goodboyneon03@gmail.com>
2023-01-08 12:32:06 +0545

    Changes

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ function setupNvim() {
   echo "Now setting up Neovim..."
   echo "The following step would open nvim and start synchronizing packages. After syncing packages, exit neovim with :q command"
 
-  read -p "Press enter to continue. " REPLY;
+  read -rp "Press enter to continue. " REPLY;
   REPLY=${REPLY:-continue}
   echo "";
 
@@ -37,7 +37,7 @@ function setup() {
 
   echo ""
 
-  read -p "Which AUR helper do you want to use? (1 = yay | 2 = paru): " REPLY
+  read -rp "Which AUR helper do you want to use? (1 = yay | 2 = paru): " REPLY
   if [[ $REPLY =~ 1 ]]; then
     AUR="yay"
   elif [[ $REPLY =~ 2 ]]; then
@@ -58,7 +58,7 @@ function setup() {
   git clone https://github.com/GoodBoyNeon/dotfiles.git
   
   echo "The next step will replace your current configuration files with new one. Make sure you have backups if you need your current config later."
-  read -p "Continue? (Y/n)" REPLY
+  read -rp "Continue? (Y/n)" REPLY
   REPLY=${REPLY:-y}
   echo "";
 
@@ -76,7 +76,7 @@ echo "";
 echo "1. Have an yay or paru (AUR helper) installed";
 echo "2. Have backed up your current configuration";
 echo "";
-read -p "Do you meet the requirements? (Y/n): " REPLY;
+read -rp "Do you meet the requirements? (Y/n): " REPLY;
 REPLY=${REPLY:-y}
 echo "";
 
